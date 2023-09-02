0314ecee25a223f7d5a717eee796abee58f80ae9
farmonovsinoo <farmonovsinolaptop@gmail.com>
2023-09-02 00:20:51 -0400

    commit

--- script.js
+++ script.js
@@ -8,9 +8,9 @@ const services = document.querySelector('.services'),
     body = document.getElementsByTagName('body');
 
 services.onclick = () => {
-    body.style.backgraound = '#081b29';
+    body.style.backgraoundimage = '';
     info.style.visibility = 'initial';
-    active.style.color = '#ededed'
+    active.style.color = '#ededed';
     services.style.color = '#00abf0';
     contact.style.color = '#ededed';
     about.style.color = '#ededed';
