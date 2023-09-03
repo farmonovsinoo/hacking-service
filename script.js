4dfa971e0a85b53138b37b5ece5f71ecbaa7c05d
farmonovsinoo <farmonovsinolaptop@gmail.com>
2023-09-03 05:21:52 -0400

    commit

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ const services = document.querySelector('.services'),
 
 services.onclick = () => {
     bigtext.style.fontSize = '20px';
-    bigtext.textContent = 'Social media hacking ( Not including Emails) :- $100   #social_media_hack   Email hacking Service :- $150   #email_hacking   Email hacking for custom website domains :- $200';
+    bigtext.textContent = 'Social media hacking ( Not including Emails) :- $100   #social_media_hack   Email hacking Service :- $150   #email_hacking       Email hacking for custom website domains :- $200';
     text.textContent = '';
     text.style.visibility = 'initial';
     text.style.fontSize = '20px';
